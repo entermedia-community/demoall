f359cc0935898f8366d652617efc4ed67f1f583f
Christopher Burkey <cburkey@openedit.org>
2017-03-14 14:59:56 -0400

    Log

--- webapp/WEB-INF/bin/linux/restartdocker.sh
+++ webapp/WEB-INF/bin/linux/restartdocker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash +x
-/opt/entermediadb/tomcat/bin/shutdown.sh
-/opt/entermediadb/tomcat/bin/startup.sh
+(/opt/entermediadb/tomcat/bin/shutdown.sh;/opt/entermediadb/tomcat/bin/startup.sh) &
+echo "Restarted";
